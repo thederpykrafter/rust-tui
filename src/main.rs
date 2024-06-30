09b7d285b54e00bd8123d980b7d9c0b1cb31c387
thederpykrafter <thederpykrafter@gmail.com>
2024-06-30 12:33:30 -0400

    basic colored text

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-fn color(color: str, text: str) {
-    println!("{}{}{}", color, text, "\x1b[0m")
-}
-
 fn main() {
-    color("\x1b[94m", "Hello, world!");
+    println!("{}{}{}","\x1b[94m", "Hello, world!", "\x1b[0m");
 }
