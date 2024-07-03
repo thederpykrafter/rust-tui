a4284219bcad81fccd512a1eb6d36f05f8b70506
thederpykrafter <thederpykrafter@gmail.com>
2024-07-03 15:40:07 -0400

    print

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-fn cprint(color: &str, text: &str) {
+fn cprintln(color: &str, text: &str) {
     println!("{}{}{}", color, text, "\x1b[0m");
 }
 
+fn cprint(color: &str, text: &str) {
+    print!("{}{}{}", color, text, "\x1b[0m");
+}
+
 fn main() {
-    cprint("\x1b[94m", "Hello World!")
+    cprint("\x1b[94m", "Hello World!");
+    cprintln("\x1b[94m", "Hello World!")
 }
